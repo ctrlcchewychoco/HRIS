b8995031d328366b459910fb266e100656c1f8d9
amiel0606 <lapid.167399@dasmarinas.sti.edu.ph>
2025-05-21 05:13:38 +0800

    ok na talaga haha

--- controller/logout.php
+++ controller/logout.php
@@ -4,16 +4,17 @@
 	
 	$dbs = new database();
 	$db=$dbs->connection();
-	$emp = $_SESSION['User']['EmployeeId'];
 	
-	if($emp==1)
-	{
-		unset($_SESSION['User']);
-		session_destroy();
-		header('location:../index.php');exit;
+	if (isset($_SESSION['User']['EmployeeId'])) {
+		$emp = $_SESSION['User']['EmployeeId'];
+	} else {
+		$emp = null;
 	}
-	else
-	{
+	
+	if ($emp == 1) {
+		// Admin logout, no need to update DB
+	} else if ($emp) {
+		// Non-admin: update logout time and workload
 		$email = $_SESSION['User']['Email'];
 		date_default_timezone_set("Asia/Kolkata");
 		$datetime = date("Y-m-d H:i:s");
@@ -21,29 +22,48 @@
 		$empid = $_SESSION['User']['EmployeeId'];
 		mysqli_query($db,"update employee set LastLogout='$datetime' where Email='$email' ");
 		
-		$datetimeid = $_SESSION['dailyid'];
-		$logindateid = mysqli_query($db,"select * from dailyworkload where DailyWorkLoadId='$datetimeid'");	
-		$LoginDate = mysqli_fetch_assoc($logindateid);
-		$logind = $LoginDate['LoginDate'];
+		if (isset($_SESSION['dailyid'])) {
+			$datetimeid = $_SESSION['dailyid'];
+			$logindateid = mysqli_query($db,"select * from dailyworkload where DailyWorkLoadId='$datetimeid'");	
+			$LoginDate = mysqli_fetch_assoc($logindateid);
+			$logind = $LoginDate['LoginDate'];
 
-		/*hours count*/
-		$date1 = date($logind);
-		$date2 = date($datetime);
-		$hours = ((strtotime($date2) - strtotime($date1))/60);
-		/*end */ 
+			/*hours count*/
+			$date1 = date($logind);
+			$date2 = date($datetime);
+			$hours = ((strtotime($date2) - strtotime($date1))/60);
+			/*end */ 
 
-		//print_r($hours);exit;
+			/*Day count*/
+			$date11=date_create($date1);
+			$date22=date_create($date2);
+			$diff=date_diff($date11,$date22);
+			$n = $diff->format("%a");
+			/*end */
 
-		/*Day count*/
-		$date11=date_create($date1);
-		$date22=date_create($date2);
-		$diff=date_diff($date11,$date22);
-		$n = $diff->format("%a");
-		/*end */
-
-		mysqli_query($db,"update dailyworkload set LogoutDate='$datetime',DailyWorkingminutes='$hours' where EmpId='$empid' and cast(LoginDate as date) = '$date'");
-		unset($_SESSION['User']);
-		session_destroy();
-		header('location:../user/index.php');exit;
+			mysqli_query($db,"update dailyworkload set LogoutDate='$datetime',DailyWorkingminutes='$hours' where EmpId='$empid' and cast(LoginDate as date) = '$date'");
+		}
 	}
-?>
+
+	// Unset all session variables
+	$_SESSION = array();
+
+	// If session uses cookies, delete the session cookie
+	if (ini_get("session.use_cookies")) {
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() - 42000,
+			$params["path"], $params["domain"],
+			$params["secure"], $params["httponly"]
+		);
+	}
+
+	// Destroy the session
+	session_destroy();
+
+	// Redirect
+	if ($emp == 1) {
+		header('Location: ../index.php');
+	} else {
+		header('Location: ../user/index.php');
+	}
+	exit;
